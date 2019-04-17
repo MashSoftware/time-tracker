89d8c0be9d3100e4d492d2d18027afb768db4e21
Matt Shaw <matthew.shaw@digital.landregistry.gov.uk>
2019-04-17 23:20:56 +0100

    set up sqlalchemy and alembic

--- config.py
+++ config.py
@@ -3,3 +3,5 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or '!DAyH2qdEqmGzriZMvxU!wzTWql6UJ4P'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgres://clickuser:clickpassword@localhost:5432/click'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
