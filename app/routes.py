767d641c91b73823c5ae39adb0fa8dac5f986f63
Matt Shaw <matthew.shaw@digital.landregistry.gov.uk>
2019-04-17 09:29:11 +0100

    signup form

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template
 
 from app import app
-from app.forms import LoginForm
+from app.forms import LoginForm, SignupForm
 
 
 @app.route('/')
@@ -9,7 +9,13 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/signup')
+def signup():
+    form = SignupForm()
+    return render_template('sign_up.html', title='Sign up', form=form)
+
+
 @app.route('/login')
 def login():
     form = LoginForm()
-    return render_template('log_in.html', form=form)
+    return render_template('log_in.html', title='Log in', form=form)
