84b8f019fe962b76764dc1b35723f5a3574666ae
Matt Shaw <matthew.shaw@digital.landregistry.gov.uk>
2019-04-18 11:49:30 +0100

    set postgres connection pool size to match heroku

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or '!DAyH2qdEqmGzriZMvxU!wzTWql6UJ4P'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgres://clickuser:clickpassword@localhost:5432/click'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_POOL_SIZE = 20
