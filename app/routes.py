bef56349672f09c8e4c3cc0cd91a632067fbe9d4
Matt Shaw <matthew.shaw@digital.landregistry.gov.uk>
2019-04-18 10:46:46 +0100

    create new user and commit to db

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from flask import flash, redirect, render_template, url_for
 
-from app import app
+from app import app, db
 from app.forms import LoginForm, SignupForm
+from app.models import User
 
 
 @app.route('/')
@@ -13,6 +14,14 @@ def index():
 def signup():
     form = SignupForm()
     if form.validate_on_submit():
+        user = User(
+            first_name=form.first_name.data,
+            last_name=form.last_name.data,
+            email_address=form.email_address.data,
+            password=form.password.data
+        )
+        db.session.add(user)
+        db.session.commit()
         flash('Thanks for signing up {}!'.format(form.first_name.data.title()), 'success')
         return redirect(url_for('index'))
     return render_template('sign_up.html', title='Sign up', form=form)
