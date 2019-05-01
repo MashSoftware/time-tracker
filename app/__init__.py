2dce8dd7ad0d5219b1c06f1f15138fcce6fad2fd
Matt Shaw <matthew.shaw@digital.landregistry.gov.uk>
2019-05-01 14:16:33 +0100

    use momentjs for locale aware timestamps

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,5 +12,6 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 login.login_message_category = 'info'
+moment = Moment(app)
 
 from app import routes, models, errors
