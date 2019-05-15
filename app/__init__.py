44f66d1167560ac7f596286f86c8600da20ce7f4
Matt Shaw <matthew.shaw@digital.landregistry.gov.uk>
2019-05-15 13:05:14 +0100

    fresh login flash message

--- app/__init__.py
+++ app/__init__.py
@@ -13,6 +13,7 @@ login.login_view = 'login'
 login.login_message_category = 'info'
 login.refresh_view = 'login'
 login.needs_refresh_message_category = 'info'
+login.needs_refresh_message = 'To protect your account, please log in again to access this page.'
 app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
 
