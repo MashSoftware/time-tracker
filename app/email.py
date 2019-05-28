5ccf9faf385f1c8fb7a93b2c61087c0ca83c599b
Matt Shaw <matthew.shaw@digital.landregistry.gov.uk>
2019-05-28 15:35:09 +0100

    async email sending

--- app/email.py
+++ app/email.py
@@ -1,14 +1,21 @@
+from threading import Thread
+
 from flask import render_template
 from flask_mail import Message
 
-from app import mail
+from app import app, mail
+
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
 
 
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    mail.send(msg)
+    Thread(target=send_async_email, args=(app, msg)).start()
 
 
 def send_reset_password_email(user):
