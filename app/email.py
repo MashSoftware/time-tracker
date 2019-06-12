94005b175625e6dd4fd542f9765d8a91e71af2e9
Matt Shaw <matthew.shaw@digital.landregistry.gov.uk>
2019-06-12 15:40:33 +0100

    use mailgun api to send email

--- app/email.py
+++ app/email.py
@@ -1,25 +1,19 @@
 import requests
+from flask import render_template
 
 from app import app
 
-# def send_reset_password_email(user):
-#     token = user.get_reset_password_token()
-#     send_email(
-#         'The Button - Reset your password',
-#         sender='the-button@mashsoftware.com',
-#         recipients=[user.email_address],
-#         text_body=render_template('email/reset_password.txt', user=user, token=token),
-#         html_body=render_template('email/reset_password.html', user=user, token=token)
-#     )
 
-
-def send_simple_message():
+def send_reset_password_email(user):
+    token = user.get_reset_password_token()
+    print(token)
     return requests.post(
         '{0}/messages'.format(app.config['MAILGUN_API_URL']),
         auth=("api", app.config['MAILGUN_API_KEY']),
         data={
             "from": "The Button <mailgun@sandboxa445b3262e834e028a1c45d00bc714b1.mailgun.org>",
-            "to": "matt@mashsoftware.com",
-            "subject": "Hello",
-            "text": "Testing some Mailgun awesomness!"}
+            "to": user.email_address,
+            "subject": "Reset your password",
+            "text": render_template('email/reset_password.txt', token=token),
+            "html": render_template('email/reset_password.html', token=token)}
     )
--- app/routes.py
+++ app/routes.py
@@ -9,7 +9,7 @@ from werkzeug.exceptions import Forbidden
 from werkzeug.urls import url_parse
 
 from app import app, db, limiter
-from app.email import send_simple_message
+from app.email import send_reset_password_email
 from app.forms import (AccountForm, EventForm, LoginForm, PasswordForm,
                        ResetPasswordForm, ResetPasswordRequestForm, SignupForm)
 from app.models import Event, User
@@ -83,16 +83,14 @@ def logout():
 
 @app.route('/reset-password-request', methods=['GET', 'POST'])
 def reset_password_request():
-    r = send_simple_message()
-    print(r)
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = ResetPasswordRequestForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email_address=form.email_address.data).first()
         if user:
-            # Send email
-            pass
+            send_reset_password_email(user)
+            print("Sent email")
         flash('Check your email for the instructions to reset your password.', 'success')
         return redirect(url_for('login'))
     return render_template('reset_password_request_form.html', title='Reset password', form=form)
