2770930b219c4078b87716327d42900c4a54f600
Matt Shaw <matthew.shaw@digital.landregistry.gov.uk>
2019-07-26 13:33:14 +0100

    validate duplicate tags

--- app/tag/forms.py
+++ app/tag/forms.py
@@ -1,6 +1,7 @@
+from flask_login import current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import InputRequired, Length
+from wtforms.validators import InputRequired, Length, ValidationError
 
 
 class TagForm(FlaskForm):
@@ -10,3 +11,8 @@ class TagForm(FlaskForm):
             InputRequired(message="Description is required"),
             Length(max=64, message="Description must be less than 64 characters")
         ])
+
+    def validate_description(self, description):
+        for tag in current_user.tags:
+            if tag.description == description.data:
+                raise ValidationError('You have already created a tag with that description')
