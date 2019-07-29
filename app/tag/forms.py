786dfe79a411c9c4058591bfd2be312b7ce4d670
Matt Shaw <matthew.shaw@digital.landregistry.gov.uk>
2019-07-29 10:11:47 +0100

    add description help text

--- app/tag/forms.py
+++ app/tag/forms.py
@@ -9,8 +9,8 @@ class TagForm(FlaskForm):
         'Name',
         validators=[
             InputRequired(message="Name is required"),
-            Length(max=64, message="Name must be less than 64 characters")
-        ])
+            Length(max=64, message="Name must be less than 64 characters")],
+        description="Must be less than 64 characters.")
 
     def validate_name(self, name):
         for tag in current_user.tags:
