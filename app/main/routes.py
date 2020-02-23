40a2d6f5c182664cacf17f484cff132c0571a951
Matt Shaw <matthew.shaw@digital.landregistry.gov.uk>
2020-02-23 22:41:30 +0000

    add favicon to base template

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,7 +1,4 @@
-import os
-
-from flask import (current_app, redirect, render_template, send_from_directory,
-                   url_for)
+from flask import redirect, render_template, url_for
 from flask_login import current_user
 from werkzeug.exceptions import HTTPException
 
@@ -13,12 +10,6 @@ def index():
     if current_user.is_authenticated:
         return redirect(url_for('entry.entries'))
     return render_template('index.html')
-
-
-@bp.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(current_app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/x-icon')
 
 
 @bp.route('/help')
