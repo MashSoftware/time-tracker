ce8afba9d66cc85e92f71c3d17d44e25408cfd40
Matt Shaw <matthew.shaw@digital.landregistry.gov.uk>
2020-02-23 22:27:32 +0000

    change favicon mimetype

--- app/main/routes.py
+++ app/main/routes.py
@@ -18,7 +18,7 @@ def index():
 @bp.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(current_app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+                               'favicon.ico', mimetype='image/x-icon')
 
 
 @bp.route('/help')
