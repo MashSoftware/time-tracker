2ef6979f55775f9d296a170bd42bf2b4a44057a4
Matt Shaw <matthew.shaw@digital.landregistry.gov.uk>
2020-02-24 16:06:08 +0000

    service files from root

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,4 +1,7 @@
-from flask import redirect, render_template, url_for
+import os
+
+from flask import (current_app, redirect, render_template, send_from_directory,
+                   url_for)
 from flask_login import current_user
 from werkzeug.exceptions import HTTPException
 
@@ -10,6 +13,24 @@ def index():
     if current_user.is_authenticated:
         return redirect(url_for('entry.entries'))
     return render_template('index.html')
+
+
+@bp.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(current_app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/x-icon')
+
+
+@bp.route('/apple-touch-icon.png')
+def apple_touch_icon():
+    return send_from_directory(os.path.join(current_app.root_path, 'static'),
+                               'apple-touch-icon.png', mimetype='image/png')
+
+
+@bp.route('/browserconfig.xml')
+def browserconfig():
+    return send_from_directory(os.path.join(current_app.root_path, 'static'),
+                               'browserconfig.xml', mimetype='application/xml')
 
 
 @bp.route('/help')
