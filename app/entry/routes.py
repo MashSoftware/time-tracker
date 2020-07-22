03653b3afedd23483be88c4a5582be99633bf758
Matt Shaw <matthew.shaw@digital.landregistry.gov.uk>
2020-07-22 15:57:41 +0100

    use class methods and black formatting

--- app/entry/routes.py
+++ app/entry/routes.py
@@ -11,11 +11,11 @@ from app.entry.forms import EventForm
 from app.models import Event
 
 
-@bp.route('/')
+@bp.route("/")
 @login_required
 @limiter.limit("1 per second", key_func=lambda: current_user.id)
 def entries():
-    page = request.args.get('page', 1, type=int)
+    page = request.args.get("page", 1, type=int)
     events = Event.query.filter_by(user_id=current_user.id).order_by(Event.started_at.desc()).paginate(page, 10, True)
     last_event = Event.query.filter_by(user_id=current_user.id).order_by(Event.started_at.desc()).first()
     if (last_event and last_event.ended_at) or last_event is None:
@@ -30,42 +30,35 @@ def entries():
             event.ended_at = event.ended_at.astimezone(pytz.timezone(current_user.timezone))
 
         date_dict = int_dict.setdefault(
-            event.started_at.strftime('%A %d %B %Y'),
-            {'entries': [], 'total_seconds': 0, 'total_duration': None, 'total_duration_decimal': 0})
-        date_dict['entries'].append(event)
+            event.started_at.strftime("%A %d %B %Y"),
+            {"entries": [], "total_seconds": 0, "total_duration": None, "total_duration_decimal": 0},
+        )
+        date_dict["entries"].append(event)
         if event.ended_at:
-            event.total_seconds = int((event.ended_at - event.started_at).total_seconds())
-            hours, remainder = divmod(event.total_seconds, 3600)
+            date_dict["total_seconds"] += event.duration()
+            hours, remainder = divmod(date_dict["total_seconds"], 3600)
             minutes, seconds = divmod(remainder, 60)
             if hours > 0:
-                event.duration = str(hours) + "h " + str(minutes) + "min"
+                date_dict["total_duration"] = str(hours) + " h " + str(minutes) + "min"
             elif minutes > 0:
-                event.duration = str(minutes) + "min"
+                date_dict["total_duration"] = str(minutes) + " min"
             else:
-                event.duration = str(seconds) + "s"
-            event.duration_decimal = (event.total_seconds / 60 / 60)
-
-            date_dict['total_seconds'] += event.total_seconds
-            hours, remainder = divmod(date_dict['total_seconds'], 3600)
-            minutes, seconds = divmod(remainder, 60)
-            if hours > 0:
-                date_dict['total_duration'] = str(hours) + "h " + str(minutes) + "min"
-            elif minutes > 0:
-                date_dict['total_duration'] = str(minutes) + "min"
-            else:
-                date_dict['total_duration'] = str(seconds) + "s"
-            date_dict['total_duration_decimal'] += event.duration_decimal
+                date_dict["total_duration"] = str(seconds) + "s"
+            date_dict["total_duration_decimal"] += event.duration_decimal()
 
     output = [
         {
-            'date': key, 'entries': value['entries'],
-            'total_duration': value['total_duration'],
-            'total_duration_decimal': value['total_duration_decimal']
-        } for key, value in int_dict.items()]
-    return render_template('entry/entries.html', events=events, start=start, entries=output)
+            "date": key,
+            "entries": value["entries"],
+            "total_duration": value["total_duration"],
+            "total_duration_decimal": value["total_duration_decimal"],
+        }
+        for key, value in int_dict.items()
+    ]
+    return render_template("entry/entries.html", events=events, start=start, entries=output)
 
 
-@bp.route('/auto')
+@bp.route("/auto")
 @login_required
 @limiter.limit("1 per second", key_func=lambda: current_user.id)
 def auto():
@@ -77,23 +70,25 @@ def auto():
         if current_user.entry_limit <= len(current_user.events):
             oldest_event = Event.query.filter_by(user_id=current_user.id).order_by(Event.started_at.asc()).first()
             db.session.delete(oldest_event)
-            flash('You have reached the {0} entry limit for your account. The oldest entry has been deleted.'
-                  .format(current_user.entry_limit), 'warning')
-        event = Event(
-            user_id=current_user.id,
-            started_at=pytz.utc.localize(datetime.utcnow()))
+            flash(
+                "You have reached the {0} entry limit for your account. The oldest entry has been deleted.".format(
+                    current_user.entry_limit
+                ),
+                "warning",
+            )
+        event = Event(user_id=current_user.id, started_at=pytz.utc.localize(datetime.utcnow()))
         db.session.add(event)
     db.session.commit()
-    return redirect(url_for('entry.entries'))
+    return redirect(url_for("entry.entries"))
 
 
-@bp.route('/manual', methods=['GET', 'POST'])
+@bp.route("/manual", methods=["GET", "POST"])
 @login_required
 @limiter.limit("1 per second", key_func=lambda: current_user.id)
 def manual():
     form = EventForm()
     form.tag.choices = [(tag.id, tag.name) for tag in current_user.tags]
-    form.tag.choices.insert(0, ('None', 'None'))
+    form.tag.choices.insert(0, ("None", "None"))
     if form.validate_on_submit():
         started_at = datetime(
             form.started_at_date.data.year,
@@ -101,13 +96,11 @@ def manual():
             form.started_at_date.data.day,
             form.started_at_time.data.hour,
             form.started_at_time.data.minute,
-            form.started_at_time.data.second
+            form.started_at_time.data.second,
         )
         locale_started_at = pytz.timezone(current_user.timezone).localize(started_at)
         utc_started_at = locale_started_at.astimezone(pytz.utc)
-        event = Event(
-            user_id=current_user.id,
-            started_at=utc_started_at)
+        event = Event(user_id=current_user.id, started_at=utc_started_at)
         if form.ended_at_date.data and form.ended_at_time.data:
             ended_at = datetime(
                 form.ended_at_date.data.year,
@@ -115,23 +108,23 @@ def manual():
                 form.ended_at_date.data.day,
                 form.ended_at_time.data.hour,
                 form.ended_at_time.data.minute,
-                form.ended_at_time.data.second
+                form.ended_at_time.data.second,
             )
             locale_ended_at = pytz.timezone(current_user.timezone).localize(ended_at)
             utc_ended_at = locale_ended_at.astimezone(pytz.utc)
             event.ended_at = utc_ended_at
         else:
             event.ended_at = None
-        if form.tag.data != 'None':
+        if form.tag.data != "None":
             event.tag_id = form.tag.data
         db.session.add(event)
         db.session.commit()
-        flash('Time entry has been added.', 'success')
-        return redirect(url_for('entry.entries'))
-    return render_template('entry/create_entry_form.html', title='Create time entry', form=form)
+        flash("Time entry has been added.", "success")
+        return redirect(url_for("entry.entries"))
+    return render_template("entry/create_entry_form.html", title="Create time entry", form=form)
 
 
-@bp.route('/<uuid:id>', methods=['GET', 'POST'])
+@bp.route("/<uuid:id>", methods=["GET", "POST"])
 @login_required
 @limiter.limit("1 per second", key_func=lambda: current_user.id)
 def update(id):
@@ -140,7 +133,7 @@ def update(id):
         raise Forbidden()
     form = EventForm()
     form.tag.choices = [(tag.id, tag.name) for tag in current_user.tags]
-    form.tag.choices.insert(0, ('None', 'None'))
+    form.tag.choices.insert(0, ("None", "None"))
     if form.validate_on_submit():
         started_at = datetime(
             form.started_at_date.data.year,
@@ -148,7 +141,7 @@ def update(id):
             form.started_at_date.data.day,
             form.started_at_time.data.hour,
             form.started_at_time.data.minute,
-            form.started_at_time.data.second
+            form.started_at_time.data.second,
         )
         locale_started_at = pytz.timezone(current_user.timezone).localize(started_at)
         utc_started_at = locale_started_at.astimezone(pytz.utc)
@@ -160,22 +153,22 @@ def update(id):
                 form.ended_at_date.data.day,
                 form.ended_at_time.data.hour,
                 form.ended_at_time.data.minute,
-                form.ended_at_time.data.second
+                form.ended_at_time.data.second,
             )
             locale_ended_at = pytz.timezone(current_user.timezone).localize(ended_at)
             utc_ended_at = locale_ended_at.astimezone(pytz.utc)
             event.ended_at = utc_ended_at
         else:
             event.ended_at = None
-        if form.tag.data != 'None':
+        if form.tag.data != "None":
             event.tag_id = form.tag.data
         else:
             event.tag_id = None
         db.session.add(event)
         db.session.commit()
-        flash('Time entry has been updated.', 'success')
-        return redirect(url_for('entry.entries'))
-    elif request.method == 'GET':
+        flash("Time entry has been updated.", "success")
+        return redirect(url_for("entry.entries"))
+    elif request.method == "GET":
         # Show timestamp in users localised timezone
         form.started_at_date.data = event.started_at.astimezone(pytz.timezone(current_user.timezone))
         form.started_at_time.data = event.started_at.astimezone(pytz.timezone(current_user.timezone))
@@ -184,10 +177,10 @@ def update(id):
             form.ended_at_time.data = event.ended_at.astimezone(pytz.timezone(current_user.timezone))
         if event.tag_id:
             form.tag.data = event.tag_id
-    return render_template('entry/update_entry_form.html', title='Edit time entry', form=form, event=event)
+    return render_template("entry/update_entry_form.html", title="Edit time entry", form=form, event=event)
 
 
-@bp.route('/<uuid:id>/delete')
+@bp.route("/<uuid:id>/delete")
 @login_required
 @limiter.limit("1 per second", key_func=lambda: current_user.id)
 def delete(id):
@@ -196,5 +189,5 @@ def delete(id):
         raise Forbidden()
     db.session.delete(event)
     db.session.commit()
-    flash('Time entry has been deleted.', 'success')
-    return redirect(url_for('entry.entries'))
+    flash("Time entry has been deleted.", "success")
+    return redirect(url_for("entry.entries"))
