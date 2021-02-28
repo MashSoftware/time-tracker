6308c319066f923e2f9e8e499febd63e8138f2b9
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2021-02-28 01:01:15 +0000

    order results by started at desc

--- app/search/routes.py
+++ app/search/routes.py
@@ -1,9 +1,9 @@
 from app import limiter
+from app.models import Event
 from app.search import bp
+from app.search.forms import SearchForm
 from flask import render_template
 from flask_login import current_user, login_required
-from app.search.forms import SearchForm
-from app.models import Event
 
 
 @bp.route("/", methods=["GET", "POST"])
@@ -12,6 +12,10 @@ from app.models import Event
 def search():
     form = SearchForm()
     if form.validate_on_submit():
-        results = Event.query.filter(Event.comment.ilike("%{}%".format(form.query.data))).all()
+        results = (
+            Event.query.filter(Event.comment.ilike("%{}%".format(form.query.data)))
+            .order_by(Event.started_at.desc())
+            .all()
+        )
         return render_template("search/search.html", title="Search", form=form, results=results)
     return render_template("search/search.html", title="Search", form=form)
