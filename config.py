a541ec4fab5832f3fa3cba4c88e71eac84cfa271
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2021-03-25 23:01:48 +0000

    cookie security

--- config.py
+++ config.py
@@ -2,13 +2,16 @@ import os
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "!DAyH2qdEqmGzriZMvxU!wzTWql6UJ4P"
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "postgresql://mash:mash@localhost:5432/button"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_ENGINE_OPTIONS = {"pool_size": 20}
-    RATELIMIT_STORAGE_URL = os.environ.get("REDIS_URL") or "memory://"
-    RATELIMIT_HEADERS_ENABLED = True
     MAILGUN_API_KEY = os.environ.get("MAILGUN_API_KEY")
     MAILGUN_API_URL = os.environ.get("MAILGUN_API_URL")
-    VERSION_NUMBER = os.environ.get("VERSION_NUMBER")
+    RATELIMIT_HEADERS_ENABLED = True
+    RATELIMIT_STORAGE_URL = os.environ.get("REDIS_URL") or "memory://"
     RELEASE_DATE = os.environ.get("RELEASE_DATE")
+    REMEMBER_COOKIE_HTTPONLY = True
+    REMEMBER_COOKIE_SECURE = True
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "!DAyH2qdEqmGzriZMvxU!wzTWql6UJ4P"
+    SESSION_COOKIE_SECURE = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "postgresql://mash:mash@localhost:5432/button"
+    SQLALCHEMY_ENGINE_OPTIONS = {"pool_size": 20}
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    VERSION_NUMBER = os.environ.get("VERSION_NUMBER")
