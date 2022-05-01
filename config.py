b9af13d24e5418670dfac2914cffde1feeeda4df
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2022-05-01 23:42:59 +0100

    remove release date and default key

--- config.py
+++ config.py
@@ -6,11 +6,10 @@ class Config(object):
     MAILGUN_API_URL = os.environ.get("MAILGUN_API_URL")
     RATELIMIT_HEADERS_ENABLED = True
     RATELIMIT_STORAGE_URL = os.environ.get("REDIS_URL") or "memory://"
-    RELEASE_DATE = os.environ.get("RELEASE_DATE")
     REMEMBER_COOKIE_DURATION = 2592000
     REMEMBER_COOKIE_HTTPONLY = True
     REMEMBER_COOKIE_SECURE = True
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "!DAyH2qdEqmGzriZMvxU!wzTWql6UJ4P"
+    SECRET_KEY = os.environ.get("SECRET_KEY")
     SESSION_COOKIE_HTTPONLY = True
     SESSION_COOKIE_SECURE = True
     SQLALCHEMY_DATABASE_URI = (
