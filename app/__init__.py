fda226c379a32d1dcce9ecbcb8ea8645b95816ee
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2022-08-02 19:11:27 +0100

    single style-src

--- app/__init__.py
+++ app/__init__.py
@@ -37,7 +37,7 @@ def create_app(config_class=Config):
     # Set content security policy
     csp = {
         "default-src": "'self'",
-        "style-src": ["https://cdn.jsdelivr.net"],
+        "style-src": "https://cdn.jsdelivr.net",
         "font-src": "https://cdn.jsdelivr.net",
         "script-src": ["https://cdn.jsdelivr.net", "'self'"],
         "img-src": ["data:", "'self'"],
