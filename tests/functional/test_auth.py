4aaf3b505647764c8b2b73921fe8f8d289862b39
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2022-08-10 11:52:29 +0100

    formatting

--- tests/functional/test_auth.py
+++ tests/functional/test_auth.py
@@ -2,9 +2,11 @@ def test_get_signup_page(client):
     response = client.get("/signup")
     assert response.status_code == 200
 
+
 def test_get_login_page(client):
     response = client.get("/login")
     assert response.status_code == 200
+
 
 def test_get_reset_password_page(client):
     response = client.get("/reset-password")
