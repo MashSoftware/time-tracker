0ae148ac8992f11e52f30004f9d0047307331de9
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2022-08-13 23:14:16 +0100

    test redirect to login

--- tests/functional/test_entry.py
+++ tests/functional/test_entry.py
@@ -1,3 +1,4 @@
 def test_get_unauthenticated_entries_page(client):
-    response = client.get("/entries")
-    assert response.status_code == 308
+    response = client.get("/entries", follow_redirects=True)
+    assert response.status_code == 200
+    assert response.request.path == "/login"
--- tests/functional/test_tag.py
+++ tests/functional/test_tag.py
@@ -1,3 +1,4 @@
 def test_get_unauthenticated_tags_page(client):
-    response = client.get("/tags")
-    assert response.status_code == 308
+    response = client.get("/tags", follow_redirects=True)
+    assert response.status_code == 200
+    assert response.request.path == "/login"
