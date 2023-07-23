89cc69ab05bb038779d299b4ac2033bf8d2d535c
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2023-07-23 17:40:25 +0100

    type annotations

--- app/utils.py
+++ app/utils.py
@@ -1,7 +1,7 @@
 from datetime import time
 
 
-def seconds_to_string(seconds):
+def seconds_to_string(seconds: int) -> str:
     """Returns the number of seconds formated as a string"""
     hours, remainder = divmod(seconds, 3600)
     minutes, seconds = divmod(remainder, 60)
@@ -13,7 +13,7 @@ def seconds_to_string(seconds):
         return str(seconds) + "s"
 
 
-def seconds_to_decimal(seconds):
+def seconds_to_decimal(seconds: int) -> float:
     """Returns the number of seconds as a float.
 
     Decimal places are cut off at two, with no rounding.
@@ -24,7 +24,7 @@ def seconds_to_decimal(seconds):
     return float(".".join(decimal))
 
 
-def seconds_to_time(seconds):
+def seconds_to_time(seconds: int) -> time:
     """Returns a datetime.time instance for a given number of seconds"""
     hours, remainder = divmod(seconds, 3600)
     minutes, seconds = divmod(remainder, 60)
