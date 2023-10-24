792cebf3fc77135bda626f1a0b56216c557726f7
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2023-10-24 10:17:09 +0100

    flake8 fixes

--- tests/unit/test_location.py
+++ tests/unit/test_location.py
@@ -7,5 +7,5 @@ def test_new_location(client, new_user):
     new_location = Location(user_id=new_user.id, name="New Location")
     assert new_location.user_id == new_user.id
     assert new_location.name == "New Location"
-    assert type(new_location.created_at) == datetime
+    assert new_location.created_at is datetime
     assert new_location.updated_at is None
