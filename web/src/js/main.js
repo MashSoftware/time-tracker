337e7471036d2bce8a49af9088d8db432a646ff5
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2025-03-09 20:54:08 +0000

    leanest css and js

--- web/src/js/main.js
+++ web/src/js/main.js
@@ -1,8 +1,19 @@
 // Import our custom CSS
 import "../scss/main.scss";
 
-// Import all of Bootstrap's JS
-import * as bootstrap from "bootstrap";
+// Import just what we need
+// import 'bootstrap/js/dist/alert';
+// import 'bootstrap/js/dist/button';
+// import 'bootstrap/js/dist/carousel';
+// import 'bootstrap/js/dist/collapse';
+// import 'bootstrap/js/dist/dropdown';
+// import 'bootstrap/js/dist/modal';
+// import 'bootstrap/js/dist/offcanvas';
+// import 'bootstrap/js/dist/popover';
+// import 'bootstrap/js/dist/scrollspy';
+// import 'bootstrap/js/dist/tab';
+// import 'bootstrap/js/dist/toast';
+// import 'bootstrap/js/dist/tooltip';
 
 // Import our custom JS
 import "./modules/cookie-banner";
