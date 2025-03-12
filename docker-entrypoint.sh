5a6769f684a12b79449de148770e3b3894c907a1
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2025-03-12 20:22:14 +0000

    stderr by default now

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 flask db upgrade
-exec gunicorn --bind 0.0.0.0:5000 -w 4 --access-logfile - --error-logfile - time_tracker:app
+exec gunicorn --bind 0.0.0.0:5000 -w 4 --access-logfile - time_tracker:app
